afee2f57af248d0d1813131524eecf914276d07f
Oleksandr <sasha2003.034@gmail.com>
2022-09-29 20:46:54 +0300

    lab1 com

---
+++ Lab1/Lab1/Lab1.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#pragma warning(disable : 4996)
+
+int main() {
+	setlocale(LC_CTYPE, "ukr");
+	int n, h, m, s;
+	printf("Введiть скiльки секунд пройшло з початку доби n=");
+	scanf("%d", &n);
+	h = n / 3600;
+	n = n % 3600;
+	m = n / 60;
+	n = n % 60;
+	s = n;
+	printf("\nЗ початку доби пройшло %d годин %d хвилин %d секунд\n", h, m, s);
+}
