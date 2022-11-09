92591017ecd083d2f0e69ace145aba1d47bbe3af
Oleksandr <sasha2003.034@gmail.com>
2022-11-09 23:46:32 +0200

    lab5

---
+++ Lab5/Lab5_1.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#pragma warning(disable : 4996)
+
+int main() {
+	setlocale(LC_CTYPE, "ukr");
+
+	int n;
+	printf("Введiть кiлькiсть елементiв в масивi: ");
+	printf("\nn = ");
+	scanf("%d", &n);
+
+	int a['n'];
+	printf("\nВвести дiйснi числа для масиву:\n");
+	for (int i = 0; i < n; i++)
+	{
+		printf("a[%d] = ", i);
+		scanf("%d", &a[i]);
+	}
+
+	int b['n'];
+	for (int i = 0; i < n; i++)
+		b[i] = a[i];
+	printf("\nМасив упорядкований по зростанню:");
+	for (int i = 0; i < n; i++)
+	{
+		int min = b[i];
+		int	imin = i;
+
+		for (int j = i + 1; j < n; j++)
+			if (min > b[j])
+			{
+				min = b[j];
+				imin = j;
+			}
+		b[imin] = b[i];
+		b[i] = min;
+	}
+	for (int i = 0; i < n; i++)
+		printf("\na[%d] = %d", i, b[i]);
+}
---
+++ Lab5/Lab5_2_A.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#pragma warning(disable : 4996)
+
+int main() {
+	setlocale(LC_CTYPE, "ukr");
+
+	int n, x = 0, y = 0;
+	printf("Введiть кiлькiсть елементiв в масивi: ");
+	printf("\nn = ");
+	scanf("%d", &n);
+
+	int a['n'];
+	printf("Ввести дiйснi числа для масиву:\n");
+	for (int i = 0; i < n; i++)
+	{
+		printf("a[%d] = ", i);
+		scanf("%d", &a[i]);
+	}
+
+	for (int i = 1; i < n; i++)
+	{
+		x += a[i];
+
+	}
+	printf("\nДобуток не нульових елементiв масиву: %d\n", x);
+
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] < 0) {
+			for (int j = 1 + i; j < n; j++) {
+				y += abs(a[j]);
+			}
+			break;
+		}
+
+	}
+	printf("\nСума модулiв елементiв масиву, розташованих пiсля першого вiд’ємного елемента: %d\n", y);
+
+}
