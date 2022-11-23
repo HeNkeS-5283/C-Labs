e293eb4b2d96d0f392ae63ee103426c3940a4ae5
Oleksandr <sasha2003.034@gmail.com>
2022-11-23 17:40:05 +0200

    lab6

---
+++ Lab6/Lab6_1.c
@@ -0,0 +1,50 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#pragma warning(disable : 4996)
+
+int main() {
+	setlocale(LC_CTYPE, "ukr");
+
+	int n, m, x = 0, y = 0;
+	printf("Введiть кiлькiсть рядкiв масиву: ");
+	printf("\nn = ");
+	scanf("%d", &n);
+	printf("Введiть кiлькiсть стовпчикiв масиву: ");
+	printf("\nm = ");
+	scanf("%d", &m);
+
+	int A['n']['m'];
+	printf("\nВведiть цiлi числа для масиву:");
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) {
+			printf("\nA[%d][%d] = ", i, j);
+			scanf("%d", &A[i][j]);
+		}
+	system("cls");
+	printf("Заданий масив %dx%d\n", n, m);
+	printf("A = ");
+	for (int i = 0; i < n; i++) {
+		printf("\t{");
+		for (int j = 0; j < m; j++) {
+			printf("\t%d,", A[i][j]);
+		}
+		printf("\t}\n");
+	}
+	int b['m'];
+
+	for (int j = 0; j < m; j++) {
+		int min = 0;
+		for (int i = 0; i < n; i++) {
+			if (i == 0) min = A[i][j];
+			if (min > A[i][j])
+				min = A[i][j];
+		}
+		b[j] = min;
+	}
+
+	printf("\nВектор b елементи якого є мiнiмальними елементами кожного зi стовпцiв.");
+	printf("\nb = {");
+	for (int j = 0; j < m; j++) printf(" %d, ", b[j]);
+	printf("}\n");
+}
---
+++ Lab6/Lab6_2.c
@@ -0,0 +1,69 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#pragma warning(disable : 4996)
+
+int main() {
+	setlocale(LC_CTYPE, "ukr");
+
+	int n, m, x = 0, y = 0;
+	printf("Введiть кiлькiсть рядкiв масиву: ");
+	printf("\nn = ");
+	scanf("%d", &n);
+	printf("Введiть кiлькiсть стовпчикiв масиву: ");
+	printf("\nm = ");
+	scanf("%d", &m);
+	const float Low = -100.1, High = 100.1;
+	srand(time(0));
+
+	float a['n']['m'];
+	printf("\nЗгенерований масив %dx%d\n", n, m);
+	printf("a = ");
+	for (int i = 0; i < n; i++) {
+		printf("\t{");
+		for (int j = 0; j < m; j++) {
+			a[i][j] = Low + High * ((float)rand() / RAND_MAX);
+			printf("\t%.2f,", a[i][j]);
+		}
+		printf("\t}\n");
+	}
+
+	float max, min;
+	int imax = 0, jmax = 0, imin = 0, jmin = 0;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) {
+			if (i == 0) {
+				min = a[i][j];
+				max = a[i][j];
+			}
+			if (a[i][j] > max) {
+				max = a[i][j];
+				imax = i;
+				jmax = j;
+			}
+			if (min > a[i][j]) {
+				min = a[i][j];
+				imin = i;
+				jmin = j;
+			}
+		}
+	a[imin][jmin] = max;
+	a[imax][jmax] = min;
+	printf("\nМаксимальне число масива: %.2f\n", max);
+	printf("\nМiнiмальне число масива: %.2f\n", min);
+
+	printf("\nМасив з помiняними мiсцями її мiнiмального та максимального елемента:\n", n, m);
+	printf("a = ");
+	for (int i = 0; i < n; i++) {
+		printf("\t{");
+		for (int j = 0; j < m; j++) {
+			printf("\t%.2f,", a[i][j]);
+		}
+		printf("\t}\n");
+	}
+
+
+	return 0;
+}
