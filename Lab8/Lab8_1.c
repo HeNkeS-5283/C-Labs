fc044218522ebe4b190a0a5de79fc22144d5707d
Oleksandr <sasha2003.034@gmail.com>
2022-11-24 05:56:46 +0200

    lab8

---
+++ Lab8/Lab8_1.c
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#pragma warning(disable : 4996)
+
+int setn() {
+	int n;
+	printf("Введiть кiлькiсть рядкiв масиву який буде згенеровано: ");
+	printf("\nn = ");
+	scanf("%d", &n);
+	return n;
+}
+
+void set_arr(int n, int* a) {
+	printf("\nВвести дiйснi числа для масиву:\n");
+	for (int i = 0; i < n; i++)
+	{
+		printf("a[%d] = ", i);
+		scanf("%d", &a[i]);
+	}
+	printf("\nЗаданий масив:\n");
+	printf("а = {");
+	for (int i = 0; i < n; i++)
+	{
+		printf(" %d, ", a[i]);
+	}
+	printf("}");
+}
+
+void act(int* p, int n) {
+	int s = 0;
+	for (int i = 0; i < n; i++) {
+		if (p[i] % 2 != 0) s++;
+	}
+	printf("\nКiлькiсть непарних елементiв масиву: %d", s);
+}
+
+int main() {
+	setlocale(LC_CTYPE, "ukr");
+
+	int n = setn();
+	int a['n'];
+	int* pa = a;
+	pa = (int*)malloc(n * sizeof(int));
+	set_arr(n, pa);
+	act(pa, n);
+
+	free(pa);
+	return 0;
+
+}
---
+++ Lab8/Lab8_2.c
@@ -0,0 +1,74 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#pragma warning(disable : 4996)
+
+int setn() {
+	int n;
+	printf("Введiть кiлькiсть рядкiв масиву: ");
+	printf("\nn = ");
+	scanf("%d", &n);
+	return n;
+}
+
+int setm() {
+	int m;
+	printf("Введiть кiлькiсть стовпчикiв масиву: ");
+	printf("\nm = ");
+	scanf("%d", &m);
+	return m;
+}
+void arr_rand(int n, int m, int *a) {
+	const int Low = -100, High = 100;
+	srand(time(0));
+
+	printf("\nЗгенерований масив %dx%d\n", n, m);
+	printf("a = ");
+	for (int i = 0; i < n; i++) {
+		printf("\t{");
+		for (int j = 0; j < m; j++) {
+			*(a + i * m + j) = rand() % Low - High;
+			printf("\t%d,", *(a + i * m + j));
+		}
+		printf("\t}\n");
+	}
+}
+
+void minimal(int n, int m, int* a) {
+	int b['n'];
+
+	for (int i = 0; i < n; i++) {
+		int min = 0;
+		 for (int j = 0; j < m; j++) {
+			if (j == 0) min = *(a + i * m + j);
+			if (min > *(a + i * m + j))
+				min = *(a + i * m + j);
+		}
+		b[i] = min;
+	}
+	
+	printf("\nВектор b елементи якого є мiнiмальними елементами кожного зi рядкiв.");
+	printf("\nb = ");
+	for (int i = 0; i < n; i++) {
+	printf("\t{");
+	printf(" %d ", b[i]);
+	printf("}\n");
+	} 
+}
+
+int main() {
+	setlocale(LC_CTYPE, "ukr");
+
+	int n = setn();
+	int m = setm();
+	int a['n']['m'];
+	int* pa = a;
+	pa = (int*)malloc(n * sizeof(int) + m);
+	arr_rand(n, m, pa);
+
+	minimal(n, m, pa);
+
+	return 0;
+}
